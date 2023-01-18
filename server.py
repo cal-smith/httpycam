718da342c88283f0adac2a26b9ef5f7d5b954719
Callum Smith <callum1234moo@gmail.com>
2023-01-17 19:36:22 -0500

    move config file to os standard locations

--- server.py
+++ server.py
@@ -2,10 +2,24 @@ from aiohttp import web, MultipartWriter
 import asyncio
 import webcam
 import configparser
+import os
+import shutil
 
 config = configparser.ConfigParser(allow_no_value=True)
-config.read("config.ini")
+# borrowing from https://stackoverflow.com/a/53222876
+configpath = os.path.join(
+    os.environ.get('APPDATA') or
+    os.environ.get('XDG_CONFIG_HOME') or
+    os.path.join(os.environ['HOME'], '.config'),
+    "httpycam"
+)
+os.makedirs(configpath, exist_ok=True)
+config.read(os.path.join(configpath, "config.ini"))
 
+# no config yet, write a default one
+if len(config.sections()) == 0:
+    shutil.copy("config.ini", configpath)
+    config.read(os.path.join(configpath, "config.ini"))
 
 async def root(_request):
     """
